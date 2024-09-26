6318eab83c5a93a822ab9f99d566106a40fb5b1f
dirkarnez <smalldirkalex@gmail.com>
2024-09-26 23:02:53 +0800

    - working

--- app/index.php
+++ app/index.php
@@ -41,6 +41,18 @@ $app->get('/names/{name}', function (Request $request, Response $response, array
 });
 
 
+// a_x=[1, 1.5, 2, 2.5, 3, 3.5, 4, 4.5, 4.7,5, 5.5]
+// a_y=[1610, 1625, 1623, 1605, 1557, 1480, 1385, 1230, 1150, 1000, 500]
+
+// b_x=[4, 4.5, 4.7, 5, 5.5, 6, 6.5, 7, 7.5, 8, 8.5, 9, 9.5]
+// b_y=[1410, 1380, 1355, 1310, 1240, 1160, 1060, 950, 840, 720, 580, 430, 245]
+
+// c_x=[2.5, 3, 3.5, 4, 4.5, 4.7, 5, 5.5, 6, 6.5, 7, 7.5, 8, 8.5, 9, 9.5, 10, 10.5, 11, 11.5, 12]
+// c_y=[1635, 1630, 1625, 1620, 1600, 1588, 1570, 1530, 1480, 1430, 1370, 1300, 1230, 1140, 1060, 970, 870, 760, 630, 480, 355]
+
+// d_x=[1, 1.5, 2, 2.5, 3, 3.5, 4, 4.5, 4.7, 5, 5.5, 6, 6.5, 7, 7.5, 8, 8.5, 9, 9.5, 10, 10.5, 11, 11.5, 12, 12.5, 13, 13.5]
+// d_y=[2080, 2075, 2070, 2065, 2060, 2050, 2040, 2035, 2033, 2030, 2010, 1970, 1930, 1880, 1820, 1760, 1690, 1610, 1530, 1440, 1350, 1260, 1150, 1040, 900, 680, 200]
+
 $app->get('/', function (Request $request, Response $response, array $args) {
     // $points = [[0, sqrt(0)], [2, sqrt(2)], [3, sqrt(3)], [4, sqrt(4)]];
     
@@ -64,9 +76,6 @@ $app->get('/', function (Request $request, Response $response, array $args) {
         }
     }
 
-    $getDistance = function ($x) use ($input, $p) {
-        return sqrt(pow($x - $input[0], 2) + pow($p($x) - $input[1], 2));
-    };
 
     $minDistance = INF;
     $minDistance_x = NAN;
@@ -76,9 +85,14 @@ $app->get('/', function (Request $request, Response $response, array $args) {
             $minDistance = $distance;
             $minDistance_x = $x;
         }
-    }
+    };
 
-    $response->getBody()->write((string)json_encode([$minDistance, $minDistance_x, $p($minDistance_x) ], JSON_PRETTY_PRINT));
+    $is_closer_to_origin = function ($in, $point_in_curve) {
+        return sqrt(pow($point_in_curve[1] - 0, 2) + pow($point_in_curve[0] - 0, 2)) > sqrt(pow($in[1] - 0, 2) + pow($in[0] - 0, 2));
+    };
+    $t = $is_closer_to_origin($input, array($minDistance_x, $p($minDistance_x)));
+
+    $response->getBody()->write((string)json_encode([$minDistance, $minDistance_x, $p($minDistance_x), $t], JSON_PRETTY_PRINT));
     return $response->withHeader('Content-Type', 'application/json')->withStatus(200);
 
 
