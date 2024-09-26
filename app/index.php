2de9d8e66b75ec7ae39ffe15c1fdfd319fc1ecc4
dirkarnez <smalldirkalex@gmail.com>
2024-09-26 22:53:53 +0800

    - working

--- app/index.php
+++ app/index.php
@@ -42,10 +42,62 @@ $app->get('/names/{name}', function (Request $request, Response $response, array
 
 
 $app->get('/', function (Request $request, Response $response, array $args) {
-    $points = [[0, 1], [1, 4], [2, 9], [3, 16]];
-    $p = Interpolation\LagrangePolynomial::interpolate($points);                // input as a set of points
-    $response->getBody()->write((string)json_encode([$p(0), $p(3)], JSON_PRETTY_PRINT));
+    // $points = [[0, sqrt(0)], [2, sqrt(2)], [3, sqrt(3)], [4, sqrt(4)]];
+    
+    //$p = Interpolation\LagrangePolynomial::interpolate($points, 0);                // input as a set of points
+    function me($x) {
+        return 2 - $x;
+    }
+
+    $points = [[0, me(0)], [1, me(1)], [2, me(2)], [3, me(3)]];
+
+    $p = Interpolation\LagrangePolynomial::interpolate($points, 0);
+    
+    $input = array(2, 2);
+
+    $interpolate_end = 0;
+    for ($x = 0; $x < INF; $x += 0.00001) {
+        $y = $p($x);
+        if ($y <= 0) {
+            $interpolate_end = $x;
+            break;
+        }
+    }
+
+    $getDistance = function ($x) use ($input, $p) {
+        return sqrt(pow($x - $input[0], 2) + pow($p($x) - $input[1], 2));
+    };
+
+    $minDistance = INF;
+    $minDistance_x = NAN;
+    for ($x = 0; $x < $interpolate_end; $x += 0.00001) {
+        $distance = sqrt(pow($x - $input[0], 2) + pow($p($x) - $input[1], 2));
+        if ($distance < $minDistance) {
+            $minDistance = $distance;
+            $minDistance_x = $x;
+        }
+    }
+
+    $response->getBody()->write((string)json_encode([$minDistance, $minDistance_x, $p($minDistance_x) ], JSON_PRETTY_PRINT));
     return $response->withHeader('Content-Type', 'application/json')->withStatus(200);
+
+
+
+
+    // gx = diff fx
+    // get x as t when gx = 0
+    // hx = diff gx()
+    // if (hx) > 0
+    // calcualte d
+
+
+
+    // $root = RootFinding\BisectionMethod::solve($fx, 0, INF, 0.00001); // Solve for x where f(x) = 0
+    
+    // sqrt()
+    // $q = $p->differentiate();
+    // $r = $q->roots();
+
 });
 
 $app->run();
