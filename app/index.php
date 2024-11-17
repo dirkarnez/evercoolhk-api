0d968fe7f6e8576f5622129f8365b2f85b1a292d
dirkarnez <smalldirkalex@gmail.com>
2024-11-17 22:59:43 +0800

    - wip

--- app/index.php
+++ app/index.php
@@ -45,14 +45,14 @@ $app->get('/names/{name}', function (Request $request, Response $response, array
 $app->get('/pdf', function (Request $request, Response $response, array $args) {
     
     // create new PDF document
-    $pdf = new TCPDF(PDF_PAGE_ORIENTATION, PDF_UNIT, PDF_PAGE_FORMAT, true, 'UTF-8', false);
+    $pdf = new TCPDF('L', PDF_UNIT, PDF_PAGE_FORMAT, true, 'UTF-8', false);
 
     // set document information
-    $pdf->SetCreator(PDF_CREATOR);
-    $pdf->SetAuthor('Nicola Asuni');
-    $pdf->SetTitle('TCPDF Example 006');
-    $pdf->SetSubject('TCPDF Tutorial');
-    $pdf->SetKeywords('TCPDF, PDF, example, test, guide');
+    // $pdf->SetCreator('PDF_CREATOR');
+    // $pdf->SetAuthor('Nicola Asuni');
+    // $pdf->SetTitle('TCPDF Example 006');
+    // $pdf->SetSubject('TCPDF Tutorial');
+    // $pdf->SetKeywords('TCPDF, PDF, example, test, guide');
 
     // set default header data
     $pdf->SetHeaderData(PDF_HEADER_LOGO, PDF_HEADER_LOGO_WIDTH, PDF_HEADER_TITLE.' 006', PDF_HEADER_STRING);
@@ -61,8 +61,8 @@ $app->get('/pdf', function (Request $request, Response $response, array $args) {
     $pdf->setHeaderFont(Array(PDF_FONT_NAME_MAIN, '', PDF_FONT_SIZE_MAIN));
     $pdf->setFooterFont(Array(PDF_FONT_NAME_DATA, '', PDF_FONT_SIZE_DATA));
 
-    // set default monospaced font
-    $pdf->SetDefaultMonospacedFont(PDF_FONT_MONOSPACED);
+    // // set default monospaced font
+    // $pdf->SetDefaultMonospacedFont(PDF_FONT_MONOSPACED);
 
     // set margins
     $pdf->SetMargins(PDF_MARGIN_LEFT, PDF_MARGIN_TOP, PDF_MARGIN_RIGHT);
@@ -96,24 +96,25 @@ $app->get('/pdf', function (Request $request, Response $response, array $args) {
     // create some HTML content
     $subtable = '<table border="1" cellspacing="6" cellpadding="4"><tr><td>a</td><td>b</td></tr><tr><td>c</td><td>d</td></tr></table>';
 
+    $styleForTable_Th_Td='border: 1px solid black; border-collapse: collapse;';
     $html = '<h2>HTML TABLE:</h2>
-    <table border="1" cellspacing="3" cellpadding="4">
+    <table style="'. $styleForTable_Th_Td . '" cellpadding="4">
         <tr>
-            <th>#</th>
-            <th align="right">中文 tę łódź jeża</th>
-            <th align="left">LEFT align</th>
-            <th>中文</th>
+            <th style="'. $styleForTable_Th_Td . '" >#</th>
+            <th style="'. $styleForTable_Th_Td . '" align="right"></th>
+            <th style="'. $styleForTable_Th_Td . '" align="left">LEFT align</th>
+            <th style="'. $styleForTable_Th_Td . '" >中文</th>
         </tr>
         <tr>
-            <td>1</td>
-            <td bgcolor="#cccccc" align="center" colspan="2">A1 ex<i>amp</i>le <a href="http://www.tcpdf.org">link</a> column span. One two tree four five six seven eight nine ten.<br />line after br<br /><small>small text</small> normal <sub>subscript</sub> normal <sup>superscript</sup> normal  bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla<ol><li>first<ol><li>sublist</li><li>sublist</li></ol></li><li>second</li></ol><small color="#FF0000" bgcolor="#FFFF00">small small small small small small small small small small small small small small small small small small small small</small></td>
-            <td>4B</td>
+            <td style="'. $styleForTable_Th_Td . '">1</td>
+            <td style="'. $styleForTable_Th_Td . '"  bgcolor="#cccccc" align="center" colspan="2">A1 ex<i>amp</i>le <a href="http://www.tcpdf.org">link</a> column span. One two tree four five six seven eight nine ten.<br />line after br<br /><small>small text</small> normal <sub>subscript</sub> normal <sup>superscript</sup> normal  bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla<ol><li>first<ol><li>sublist</li><li>sublist</li></ol></li><li>second</li></ol><small color="#FF0000" bgcolor="#FFFF00">small small small small small small small small small small small small small small small small small small small small</small></td>
+            <td style="'. $styleForTable_Th_Td . '">4B</td>
         </tr>
         <tr>
-            <td>'.$subtable.'</td>
-            <td bgcolor="#0000FF" color="yellow" align="center">A2 € &euro; &#8364; &amp; è &egrave;<br/>A2 € &euro; &#8364; &amp; è &egrave;</td>
-            <td bgcolor="#FFFF00" align="left"><font color="#FF0000">Red</font> Yellow BG</td>
-            <td>4C</td>
+            <td style="'. $styleForTable_Th_Td . '">'.$subtable.'</td>
+            <td style="'. $styleForTable_Th_Td . '" bgcolor="#0000FF" color="yellow" align="center">A2 € &euro; &#8364; &amp; è &egrave;<br/>A2 € &euro; &#8364; &amp; è &egrave;</td>
+            <td style="'. $styleForTable_Th_Td . '" bgcolor="#FFFF00" align="left"><font color="#FF0000">Red</font> Yellow BG</td>
+            <td style="'. $styleForTable_Th_Td . '">4C</td>
         </tr>
         <tr>
             <td>1A</td>
