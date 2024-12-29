e2ae56b914c8372cfa5ed0617377bf49985ca2d6
Dirk Arnez <smalldirkalex@gmail.com>
2024-12-29 23:00:59 +0800

    - upload files

--- commiting.cmd
+++ commiting.cmd
@@ -7,7 +7,10 @@ C:\Windows\System32\timeout.exe /t 5 &&^
 echo committing &&^
 git config --global credential.helper "" &&^
 git add * &&^
-git commit -m "- upload files" &&^
-git push -u origin main && goto end
+git commit -m "- upload files"
+
+
+( git push -u origin main && goto end ) || C:\Windows\System32\rundll32 user32.dll,MessageBeep 
+
 
 pause
