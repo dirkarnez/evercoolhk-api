391b6bad06f0ec6e9d9f939048aaf9b89cd40349
dirkarnez <smalldirkalex@gmail.com>
2024-12-30 18:48:13 +0800

    - upload files

--- app/app/Casts/Hash.php
+++ app/app/Casts/Hash.php
@@ -29,18 +29,13 @@ class Hash implements Castable
             //     return "344";
             // }
 
-            public function get($model, string $key, $value, array $attributes);
+            public function get($model, string $key, $value, array $attributes) {
 
-            /**
-             * Transform the attribute to its underlying model values.
-             *
-             * @param  \Illuminate\Database\Eloquent\Model  $model
-             * @param  string  $key
-             * @param  mixed  $value
-             * @param  array  $attributes
-             * @return mixed
-             */
-            public function set($model, string $key, $value, array $attributes);
+            }
+
+            public function set($model, string $key, $value, array $attributes){
+                return "12cvv";
+            }
         };
     }
 }
