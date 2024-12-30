029a9315734f7aeaefb4f9edd1863d185ffde5bc
dirkarnez <smalldirkalex@gmail.com>
2024-12-31 03:21:04 +0800

    - upload files

--- app/index.php
+++ app/index.php
@@ -157,7 +157,7 @@ $app->get('/email-testing', function (Request $request, Response $response, arra
 });
 
 $app->post('/pdf', function (Request $request, Response $response, array $args) {
-    $body = json_encode($request->getBody()->getContents());
+    $body = json_decode($request->getBody()->getContents());
 
     // create new PDF document
     $pdf = new MyTCPDF('L', PDF_UNIT, PDF_PAGE_FORMAT, true, 'UTF-8', false);
