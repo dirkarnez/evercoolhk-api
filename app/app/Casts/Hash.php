1c3e34488330862d52a6d77b6969c8dae9f8bdeb
dirkarnez <smalldirkalex@gmail.com>
2024-12-30 18:50:50 +0800

    - upload files

--- app/app/Casts/Hash.php
+++ app/app/Casts/Hash.php
@@ -12,23 +12,6 @@ class Hash implements Castable
     {
         return new class implements CastsAttributes
         {
-            // public function get(Model $model, string $key, mixed $value, array $attributes): Address
-            // {
-            //     return new Address(
-            //         $attributes['address_line_one'],
-            //         $attributes['address_line_two']
-            //     );
-            // }
- 
-            // public function set(Model $model, string $key, mixed $value, array $attributes): string
-            // {
-            //     // return [
-            //     //     'address_line_one' => $value->lineOne,
-            //     //     'address_line_two' => $value->lineTwo,
-            //     // ];
-            //     return "344";
-            // }
-
             public function get($model, string $key, $value, array $attributes) {
                 return bin2hex($value);
             }
