55e10f152ea003885aeb41c7545d781176848bf3
dirkarnez <smalldirkalex@gmail.com>
2024-12-31 03:12:20 +0800

    - upload files

--- app/index.php
+++ app/index.php
@@ -157,7 +157,7 @@ $app->get('/email-testing', function (Request $request, Response $response, arra
 });
 
 $app->post('/pdf', function (Request $request, Response $response, array $args) {
-    $parsedBody = $request->getParsedBody();
+    $parsedBody = $request->getBody();
     
     
     
