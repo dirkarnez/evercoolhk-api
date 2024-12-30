f6c40f43091d9a47868348605c37adf1326a0d26
dirkarnez <smalldirkalex@gmail.com>
2024-12-30 18:48:00 +0800

    - upload files

--- app/app/Casts/Hash.php
+++ app/app/Casts/Hash.php
@@ -12,22 +12,35 @@ class Hash implements Castable
     {
         return new class implements CastsAttributes
         {
-            public function get(Model $model, string $key, mixed $value, array $attributes): Address
-            {
-                return new Address(
-                    $attributes['address_line_one'],
-                    $attributes['address_line_two']
-                );
-            }
+            // public function get(Model $model, string $key, mixed $value, array $attributes): Address
+            // {
+            //     return new Address(
+            //         $attributes['address_line_one'],
+            //         $attributes['address_line_two']
+            //     );
+            // }
  
-            public function set(Model $model, string $key, mixed $value, array $attributes): string
-            {
-                // return [
-                //     'address_line_one' => $value->lineOne,
-                //     'address_line_two' => $value->lineTwo,
-                // ];
-                return "344";
-            }
+            // public function set(Model $model, string $key, mixed $value, array $attributes): string
+            // {
+            //     // return [
+            //     //     'address_line_one' => $value->lineOne,
+            //     //     'address_line_two' => $value->lineTwo,
+            //     // ];
+            //     return "344";
+            // }
+
+            public function get($model, string $key, $value, array $attributes);
+
+            /**
+             * Transform the attribute to its underlying model values.
+             *
+             * @param  \Illuminate\Database\Eloquent\Model  $model
+             * @param  string  $key
+             * @param  mixed  $value
+             * @param  array  $attributes
+             * @return mixed
+             */
+            public function set($model, string $key, $value, array $attributes);
         };
     }
 }
