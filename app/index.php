37390519646819bb881b07c62e6377bc00887c31
dirkarnez <smalldirkalex@gmail.com>
2024-12-31 02:53:41 +0800

    - upload files

--- app/index.php
+++ app/index.php
@@ -430,7 +430,7 @@ $app->post('/pdf', function (Request $request, Response $response, array $args) 
 
     // ---------------------------------------------------------
     $response->getBody()->write($pdf->Output('123', 'I'));
-    return $response->withHeader('Content-Type', 'application/pdf')->withStatus(200);
+    return enableCORS($response->withHeader('Content-Type', 'application/pdf')->withStatus(200));
 });
 
 $app->get('/calculate', function (Request $request, Response $response, array $args) {
