65412597f9a61f1cbb7339dcce8ea77f1868198e
dirkarnez <smalldirkalex@gmail.com>
2024-12-30 18:55:41 +0800

    - upload files

--- app/index.php
+++ app/index.php
@@ -38,7 +38,8 @@ $app->get('/hi', function (Request $request, Response $response, array $args) {
 
     // }
     // select * from ahu_models where `maximum_air_volume` > 14900 ORDER BY `maximum_air_volume` LIMIT 2;
-    $actualAirVolume = 
+    $actualAirVolume = 14900;
+    
     AHUModel::where('maximum_air_volume', '>', )
 
     //     if (isset($queryParams['random_count'])) {
