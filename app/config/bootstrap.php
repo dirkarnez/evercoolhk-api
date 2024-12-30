9e7d82f4ad0d1c6a8063a6fa9cea482ef6e893a3
dirkarnez <smalldirkalex@gmail.com>
2024-12-30 17:53:57 +0800

    - upload files

--- app/config/bootstrap.php
+++ app/config/bootstrap.php
@@ -65,9 +65,9 @@ $capsule->addConnection([
     'driver'  => 'mysql',
     'host'    => $_ENV['DB_HOST'],
     'port'    => $_ENV['DB_PORT'],
-    'database'=> $_ENV['EMAIL_SENDER_ACCOUNT'],
-    'username'=> $_ENV['EMAIL_SENDER_ACCOUNT'],
-    'password'=> $_ENV['EMAIL_SENDER_ACCOUNT'],
+    'database'=> $_ENV['DB_DATABASE'],
+    'username'=> $_ENV['DB_USER_NAME'],
+    'password'=> $_ENV['DB_PASSWORD'],
     'charset' => 'utf8mb4',
     'collation'=> 'utf8mb4_unicode_ci',
     'prefix'  => ''
