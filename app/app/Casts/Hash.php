0543c70b378dea2c1f859f1c398b6b1320bdfaf7
dirkarnez <smalldirkalex@gmail.com>
2024-12-30 18:48:52 +0800

    - upload files

--- app/app/Casts/Hash.php
+++ app/app/Casts/Hash.php
@@ -30,7 +30,7 @@ class Hash implements Castable
             // }
 
             public function get($model, string $key, $value, array $attributes) {
-                return "12cvv";
+                return "12cvv" . $value;
             }
 
             public function set($model, string $key, $value, array $attributes){
