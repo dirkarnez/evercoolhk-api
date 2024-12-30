2c0c30f7ff677f7a6cdd41ab9b718fa8d1febb1a
dirkarnez <smalldirkalex@gmail.com>
2024-12-30 18:42:54 +0800

    - upload files

---
+++ app/app/Casts/Hash.php
@@ -0,0 +1,31 @@
+
+<?php
+
+namespace MyApp\Casts;
+ 
+use Illuminate\Contracts\Database\Eloquent\CastsInboundAttributes;
+use Illuminate\Database\Eloquent\Model;
+ 
+class Hash implements CastsInboundAttributes
+{
+    /**
+     * Create a new cast class instance.
+     */
+    public function __construct(
+        protected string|null $algorithm = null,
+    ) {}
+ 
+    /**
+     * Prepare the given value for storage.
+     *
+     * @param  array<string, mixed>  $attributes
+     */
+    public function set($model, string $key, $value, array $attributes)
+    {
+        return "123";
+        // return is_null($this->algorithm)
+        //             ? bcrypt($value)
+        //             : hash($this->algorithm, $value);
+    }
+}
+?>
