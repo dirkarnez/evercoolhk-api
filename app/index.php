22871e570296458658712a95afdf2d3312eb1702
dirkarnez <smalldirkalex@gmail.com>
2024-12-30 18:58:53 +0800

    - upload files

--- app/index.php
+++ app/index.php
@@ -35,7 +35,7 @@ function writeJSON(Response $response, int $statusCode, mixed $contentToEncode) 
 $app->get('/hi', function (Request $request, Response $response, array $args) {
     $queryParams = $request->getQueryParams();
     if (isset($queryParams['actual_air_volume'])) {
-        $randomCount = (int)$queryParams['random_count'];
+        $actualAirVolume = (int)$queryParams['actual_air_volume'];
     }
     // select * from ahu_models where `maximum_air_volume` > 14900 ORDER BY `maximum_air_volume` LIMIT 2;
     $actualAirVolume = isset($queryParams['actual_air_volume']) ? ;
