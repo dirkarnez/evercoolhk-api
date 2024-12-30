af74829c035d8db5bdb487f68eb70229291cd7c4
dirkarnez <smalldirkalex@gmail.com>
2024-12-31 03:25:01 +0800

    - upload files

--- app/index.php
+++ app/index.php
@@ -334,7 +334,7 @@ $app->post('/pdf', function (Request $request, Response $response, array $args) 
                 <td style="'. $styleForTable_Th_Td . '"></td>
                 <td style="'. $styleForTable_Th_Td . '"></td>
             </tr>
-            array_map('cube', $a);
+            array_map('cube', $$body);
         . ' </tbody>
     </table>
     <br>
