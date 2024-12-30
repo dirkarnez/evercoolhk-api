4e4934bcba1517d346d331e9585de879422771cd
dirkarnez <smalldirkalex@gmail.com>
2024-12-30 17:49:50 +0800

    - upload files

--- app/config/bootstrap.php
+++ app/config/bootstrap.php
@@ -63,7 +63,7 @@ $errorMiddleware = $app->addErrorMiddleware(true, true, true, null);
 $capsule = new Capsule;
 $capsule->addConnection([
     'driver'  => 'mysql',
-    'host'    => 'localhost',
+    'host'    => '',
     'database'=> 'evercoolhk_2020',
     'username'=> 'mysql2020',
     'password'=> "cahxieyooteej",
