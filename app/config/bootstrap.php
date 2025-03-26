9c43c1a0a94dd06701b01238b319ae277e191b97
dirkarnez <smalldirkalex@gmail.com>
2025-03-26 14:46:28 +0800

    - wip

--- app/config/bootstrap.php
+++ app/config/bootstrap.php
@@ -5,6 +5,9 @@ use DI\Container;
 use Slim\Factory\AppFactory;
 use Slim\App;
 use \Illuminate\Database\Capsule\Manager as Capsule;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\RequestHandlerInterface;
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
@@ -35,6 +38,32 @@ $app = AppFactory::create();
 //     $app->setBasePath('/api');
 // }
 
+$app->addBodyParsingMiddleware();
+$app->addRoutingMiddleware();
+
+// This CORS middleware will append the response header
+// Access-Control-Allow-Methods with all allowed methods
+$app->add(function (ServerRequestInterface $request, RequestHandlerInterface $handler) use ($app): ResponseInterface {
+    if ($request->getMethod() === 'OPTIONS') {
+        $response = $app->getResponseFactory()->createResponse();
+    } else {
+        $response = $handler->handle($request);
+    }
+
+    $response = $response
+        ->withHeader('Access-Control-Allow-Credentials', 'true')
+        ->withHeader('Access-Control-Allow-Origin', '*')
+        ->withHeader('Access-Control-Allow-Headers', '*')
+        ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS')
+        ->withHeader('Cache-Control', 'no-store, no-cache, must-revalidate, max-age=0')
+        ->withHeader('Pragma', 'no-cache');
+
+    if (ob_get_contents()) {
+        ob_clean();
+    }
+
+    return $response;
+});
 
 $errorMiddleware = $app->addErrorMiddleware(true, true, true, null);
 //$errorMiddleware->setDefaultErrorHandler(function () use ($app) {
